29623e6559f831939c5fdbb5ca362786e9e50225
Egomann88 <justin-urbanek@bluewin.ch>
2023-10-27 14:07:37 +0200

    refactor: rm ChildLayer redundancy

--- FillForms.js
+++ FillForms.js
@@ -7,40 +7,40 @@ function meta() {
 
   // list of answers
   console.log("The Answers are in the 6-Chilren Layer:");
-  console.log(document.getElementById("question-list").children[1].children[0].children[1].children[0].children[0].children);
+  console.log(getChildLayer(document.getElementById("question-list"), [1, 0, 1, 0, 0]).children);
 
   // random answer
   console.log("Choose a random answer:");
-  let answers = document.getElementById("question-list").children[1].children[0].children[1].children[0].children[0].children
-  answers[Math.floor(Math.random() * answers.length)].children[0].children[0].children[0].children[0].click();
+  let answers = getChildLayer(document.getElementById("question-list"), [1, 0, 1, 0, 0]).children;
+  getChildLayer(answers[Math.floor(Math.random() * answers.length)], [0, 0, 0, 0]).click();
 
   // click
   console.log("Click on the checkbox");
-  document.getElementById("question-list").children[1].children[0].children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[0].click()
+  getChildLayer(document.getElementById("question-list"), [1, 0, 1, 0, 0, 0, 0, 0, 0, 0].click());
 
   // net score is special case
   console.log("Net score is special case");
-  answers = document.getElementById("question-list").children[17].children[0].children[1].children[0].children[1].children[1].children[1].children[0].children
-  answers[Math.floor(Math.random() * answers.length)].children[0].children[0].click()
+  answers = getChildLayer(document.getElementById("question-list"), [17, 0, 1, 0, 1, 1, 1, 0]).children;
+  getChildLayer(answers[Math.floor(Math.random() * answers.length)], [0, 0]).click();
 }
 
 function fillForms(index = 0, max = 200) {
   if (index >= max) return;
 
   setTimeout(() => {
-    let questions = document.getElementById("question-list").children.length;
+    let questions = document.getElementById("question-list").children;
 
-    for (let i = 1; i < questions; i++) {
+    for (let i = 1; i < questions.length; i++) {
       if (i == 17) { // net score question
-        let answers = document.getElementById("question-list").children[17].children[0].children[1].children[0].children[1].children[1].children[1].children[0].children
-        answers[Math.floor(Math.random() * answers.length)].children[0].children[0].click()
+        let answers = getChildLayer(questions[i], [0, 1, 0, 1, 1, 1, 0]).children;
+        getChildLayer(answers[Math.floor(Math.random() * answers.length)], [0, 0]).click();
       } else {
-        let answers = document.getElementById("question-list").children[i].children[0].children[1].children[0].children[0].children;
+        let answers = getChildLayer(questions[i], [0, 1, 0, 0]).children;
 
-        if (answers[0].children[0].children[0].children[0].children[0].type == "checkbox") { // multiple choice
+        if (getChildLayer(answers[0], [0, 0, 0, 0]).type == "checkbox") { // multiple choice
           let valueBefore = null;
           for (let j = 0; j < Math.floor(Math.random() * answers.length); j++) {
-            let value = answers[Math.floor(Math.random() * answers.length)].children[0].children[0].children[0].children[0];
+            let value = getChildLayer(answers[Math.floor(Math.random() * answers.length)], [0, 0, 0, 0])
 
             if (value == valueBefore) continue;
 
@@ -48,7 +48,7 @@ function fillForms(index = 0, max = 200) {
             value.click();
           }
         } else { // single choice
-          answers[Math.floor(Math.random() * answers.length)].children[0].children[0].children[0].children[0].click();
+          getChildLayer(answers[Math.floor(Math.random() * answers.length)], [0, 0, 0, 0]).click();
         }
       }
     }
