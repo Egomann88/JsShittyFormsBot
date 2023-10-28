4d1a75505af1fd76d8837cb7c107a54d3485be35
Egomann88 <justin-urbanek@bluewin.ch>
2023-10-28 11:44:11 +0200

    feat: choose answers with propability

--- FillForms.js
+++ FillForms.js
@@ -24,31 +24,51 @@ function meta() {
   getChildLayer(answers[Math.floor(Math.random() * answers.length)], [0, 0]).click();
 }
 
-function fillForms(index = 0, max = 200) {
+function fillForms(index = 0, max = 5) {
+  document.getElementsByTagName("a")[0].remove()
   if (index >= max) return;
 
   setTimeout(() => {
     let questions = document.getElementById("question-list").children;
 
     for (let i = 1; i < questions.length; i++) {
+      let answerProps = answerPropabilities[i - 1];
+      let answer = getRndAnswer(answerProps);
+
+      console.log(i);
+
       if (i == 17) { // net score question
         let answers = getChildLayer(questions[i], [0, 1, 0, 1, 1, 1, 0]).children;
-        getChildLayer(answers[Math.floor(Math.random() * answers.length)], [0, 0]).click();
+
+        if (answer == 0) answer = Math.floor(Math.random() * 5) + 1;
+        else answer = Math.floor(Math.random() * 5) + 6;
+
+        getChildLayer(answers[answer], [0, 0]).click();
       } else {
         let answers = getChildLayer(questions[i], [0, 1, 0, 0]).children;
 
         if (getChildLayer(answers[0], [0, 0, 0, 0]).type == "checkbox") { // multiple choice
-          let valueBefore = null;
-          for (let j = 0; j < Math.floor(Math.random() * answers.length); j++) {
-            let value = getChildLayer(answers[Math.floor(Math.random() * answers.length)], [0, 0, 0, 0])
+          let loopNumber = Math.floor(Math.random() * answers.length - 1);
+          let clickedAnswers = [];
 
-            if (value == valueBefore) continue;
+          for (let j = 0; j < loopNumber; j++) {
+            let maxTries = 8;
 
-            valueBefore = value;
-            value.click();
+            while (clickedAnswers.includes(answer)) {
+              if (maxTries-- <= 0) break;
+
+              answer = getRndAnswer(answerProps);
+            }
+            if (maxTries <= 0) break;
+            console.log("ans", answer, "lengh", answers.length);
+            if (answer >= answers.length) break;
+
+            clickedAnswers.push(answer);
+            console.log("this answer is clicked:", answers[answer]);
+            getChildLayer(answers[answer], [0, 0, 0, 0]).click();
           }
         } else { // single choice
-          getChildLayer(answers[Math.floor(Math.random() * answers.length)], [0, 0, 0, 0]).click();
+          getChildLayer(answers[answer], [0, 0, 0, 0]).click();
         }
       }
     }
@@ -58,16 +78,16 @@ function fillForms(index = 0, max = 200) {
 
     // get last button
     let lastButton = buttons[buttons.length - 1];
-    lastButton.click();
-  }, 1000);
+    //lastButton.click();
+  }, 500);
 
   setTimeout(() => {
     // get "Send more Answers" text
     let links = document.getElementsByTagName("a");
-    console.log("timeout");
-    links[0].click();
-    fillForms(index + 1, max);
-  }, 1200);
+    console.log(links[0], "timeout");
+    //links[0].click();
+    //fillForms(index + 1, max);
+  }, 3200);
 }
 
 function getChildLayer(element, indexList) {
@@ -75,4 +95,59 @@ function getChildLayer(element, indexList) {
     element = element.children[indexList[i]];
   }
   return element;
+}
+
+let answerPropabilities = [
+  [25, 40, 25, 10],
+  [50, 5, 5, 25, 15],
+  [15, 25, 25, 20, 15],
+  [40, 15, 25, 5, 15],
+  [15, 15, 5, 5, 30, 30], // last is special case
+  [15, 15, 15, 15, 15, 25], // last is special case
+  [8, 20, 40, 32],
+  [5, 15, 40, 40],
+  [25, 40, 35],
+  [15, 35, 50],
+  [20, 40, 40],
+  [40, 40, 20],
+  [30, 35, 25, 10],
+  [10, 35, 30, 25],
+  [15, 15, 15, 55],
+  [10, 55, 25, 10],
+  [70, 30], // 1. unter 5 ; 2. über 5
+  [10, 30, 35, 25],
+  [30, 25, 20, 15, 7, 3],
+  [20, 50, 30],
+  [15, 20, 20, 22, 23],
+  [10, 20, 30, 25, 15],
+  [31, 27, 15, 10, 2, 0, 15],
+  [36, 33, 31],
+  [25, 30, 30, 15],
+  [40, 20, 5, 3, 20, 12],
+];
+
+function checkIfCorrect() {
+  for (let i = 0; i < answerPropabilities.length; i++) {
+    getRndAnswer(answerPropabilities[i]);
+    console.log(i + 1, answerPropabilities[i]);
+  }
+}
+
+
+function getRndAnswer(propabilityList) {
+  const allPropabilities = propabilityList.reduce((sum, p) => sum + p, 0);
+
+  if (allPropabilities !== 100) {
+    throw new Error("Die Summe der Wahrscheinlichkeiten muss 100 betragen.");
+  }
+
+  const rnd = Math.random() * 100;
+  let culmulativePropabilitiy = 0;
+
+  for (let i = 0; i < propabilityList.length; i++) {
+    culmulativePropabilitiy += propabilityList[i];
+    if (rnd <= culmulativePropabilitiy) {
+      return i;
+    }
+  }
 }
